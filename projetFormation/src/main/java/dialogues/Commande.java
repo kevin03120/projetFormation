17fb42739bc7d212076df693c6838ea9820e7897
Kevin <kevin.malleret03@gmail.com>
2017-07-25 10:45:10 +0200

    Ajout des id pour label et text field de commande

--- projetFormation/src/main/java/dialogues/Commande.java
+++ projetFormation/src/main/java/dialogues/Commande.java
@@ -27,13 +27,13 @@ import javax.swing.DefaultComboBoxModel;
 public class Commande extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTable table;
+	private JTextField txtCommande;
+	private JTextField txtDate;
+	private JTextField txtCode;
+	private JTextField txtCategorie;
+	private JTextField txtDesignation;
+	private JTextField txtMontant;
+	private JTable tblCommande;
 
 	/**
 	 * Launch the application.
@@ -71,12 +71,12 @@ public class Commande extends JFrame {
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Commande");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setIcon(new ImageIcon("C:\\images\\gestion\\commande\\Shopping-Bag-64-actif.png"));
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel.setBounds(10, 5, 230, 64);
-		panel.add(lblNewLabel);
+		JLabel lblCommande = new JLabel("Commande");
+		lblCommande.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCommande.setIcon(new ImageIcon("C:\\images\\gestion\\commande\\Shopping-Bag-64-actif.png"));
+		lblCommande.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblCommande.setBounds(10, 5, 230, 64);
+		panel.add(lblCommande);
 		
 		JLabel lblCommandeExistante = new JLabel("Commandes existantes");
 		lblCommandeExistante.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -85,33 +85,33 @@ public class Commande extends JFrame {
 		lblCommandeExistante.setBounds(10, 80, 230, 48);
 		panel.add(lblCommandeExistante);
 		
-		JLabel lblNewLabel_1 = new JLabel("Supprimer");
-		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_1.setIcon(new ImageIcon("C:\\images\\gestion\\Cancel-48.png"));
-		lblNewLabel_1.setBounds(10, 139, 230, 41);
-		panel.add(lblNewLabel_1);
+		JLabel lblSupprime = new JLabel("Supprimer");
+		lblSupprime.setForeground(new Color(255, 255, 255));
+		lblSupprime.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblSupprime.setIcon(new ImageIcon("C:\\images\\gestion\\Cancel-48.png"));
+		lblSupprime.setBounds(10, 139, 230, 41);
+		panel.add(lblSupprime);
 		
-		JLabel lblSupprimerToutesLes = new JLabel("Supprimer toutes les lignes");
-		lblSupprimerToutesLes.setIcon(new ImageIcon("C:\\images\\gestion\\Garbage-Open-48.png"));
-		lblSupprimerToutesLes.setForeground(Color.WHITE);
-		lblSupprimerToutesLes.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblSupprimerToutesLes.setBounds(10, 191, 230, 41);
-		panel.add(lblSupprimerToutesLes);
+		JLabel lblSupprimeAll = new JLabel("Supprimer toutes les lignes");
+		lblSupprimeAll.setIcon(new ImageIcon("C:\\images\\gestion\\Garbage-Open-48.png"));
+		lblSupprimeAll.setForeground(Color.WHITE);
+		lblSupprimeAll.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblSupprimeAll.setBounds(10, 191, 230, 41);
+		panel.add(lblSupprimeAll);
 		
-		JLabel lblValiderLaCommande = new JLabel("Valider la commande");
-		lblValiderLaCommande.setIcon(new ImageIcon("C:\\images\\gestion\\commande\\Shopping-Cart-05-48.png"));
-		lblValiderLaCommande.setForeground(Color.WHITE);
-		lblValiderLaCommande.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblValiderLaCommande.setBounds(10, 244, 230, 41);
-		panel.add(lblValiderLaCommande);
+		JLabel lblValider = new JLabel("Valider la commande");
+		lblValider.setIcon(new ImageIcon("C:\\images\\gestion\\commande\\Shopping-Cart-05-48.png"));
+		lblValider.setForeground(Color.WHITE);
+		lblValider.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblValider.setBounds(10, 244, 230, 41);
+		panel.add(lblValider);
 		
-		JLabel lblAperu = new JLabel("Aper\u00E7u");
-		lblAperu.setIcon(new ImageIcon("C:\\images\\gestion\\Preview-48.png"));
-		lblAperu.setForeground(Color.WHITE);
-		lblAperu.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblAperu.setBounds(10, 324, 230, 41);
-		panel.add(lblAperu);
+		JLabel lblApercu = new JLabel("Aper\u00E7u");
+		lblApercu.setIcon(new ImageIcon("C:\\images\\gestion\\Preview-48.png"));
+		lblApercu.setForeground(Color.WHITE);
+		lblApercu.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblApercu.setBounds(10, 324, 230, 41);
+		panel.add(lblApercu);
 		
 		JLabel lblImprimer = new JLabel("Imprimer");
 		lblImprimer.setIcon(new ImageIcon("C:\\images\\gestion\\Printer-48.png"));
@@ -141,41 +141,41 @@ public class Commande extends JFrame {
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
-		JLabel lblNewLabel_2 = new JLabel("Commande en cours");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_2.setBounds(10, 23, 124, 14);
-		panel_1.add(lblNewLabel_2);
+		JLabel lblCommandeCours = new JLabel("Commande en cours");
+		lblCommandeCours.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblCommandeCours.setBounds(10, 23, 124, 14);
+		panel_1.add(lblCommandeCours);
 		
-		textField = new JTextField();
-		textField.setEnabled(false);
-		textField.setBounds(144, 21, 241, 20);
-		panel_1.add(textField);
-		textField.setColumns(10);
+		txtCommande = new JTextField();
+		txtCommande.setEnabled(false);
+		txtCommande.setBounds(144, 21, 241, 20);
+		panel_1.add(txtCommande);
+		txtCommande.setColumns(10);
 		
 		JLabel lblDate = new JLabel("Date");
 		lblDate.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblDate.setBounds(418, 23, 29, 14);
 		panel_1.add(lblDate);
 		
-		textField_1 = new JTextField();
-		textField_1.setEnabled(false);
-		textField_1.setBounds(457, 21, 113, 20);
-		panel_1.add(textField_1);
-		textField_1.setColumns(10);
+		txtDate = new JTextField();
+		txtDate.setEnabled(false);
+		txtDate.setBounds(457, 21, 113, 20);
+		panel_1.add(txtDate);
+		txtDate.setColumns(10);
 		
 		JLabel lblNomDuClient = new JLabel("Nom du client");
 		lblNomDuClient.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblNomDuClient.setBounds(50, 66, 84, 14);
 		panel_1.add(lblNomDuClient);
 		
-		JLabel lblNewLabel_3 = new JLabel("Selectionner le client");
-		lblNewLabel_3.setIcon(new ImageIcon("C:\\images\\gestion\\Search-48.png"));
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_3.setBackground(new Color(255, 140, 0));
-		lblNewLabel_3.setForeground(new Color(255, 255, 255));
-		lblNewLabel_3.setBounds(144, 52, 426, 44);
-		lblNewLabel_3.setOpaque(true);
-		panel_1.add(lblNewLabel_3);
+		JLabel txtNomDuClient = new JLabel("Selectionner le client");
+		txtNomDuClient.setIcon(new ImageIcon("C:\\images\\gestion\\Search-48.png"));
+		txtNomDuClient.setFont(new Font("Tahoma", Font.BOLD, 13));
+		txtNomDuClient.setBackground(new Color(255, 140, 0));
+		txtNomDuClient.setForeground(new Color(255, 255, 255));
+		txtNomDuClient.setBounds(144, 52, 426, 44);
+		txtNomDuClient.setOpaque(true);
+		panel_1.add(txtNomDuClient);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new TitledBorder(new LineBorder(new Color(255, 140, 0)), "Valeurs de la commande", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -184,73 +184,73 @@ public class Commande extends JFrame {
 		contentPane.add(panel_2);
 		panel_2.setLayout(null);
 		
-		JLabel lblNewLabel_4 = new JLabel("Selectionner un article");
-		lblNewLabel_4.setOpaque(true);
-		lblNewLabel_4.setBackground(new Color(255, 140, 0));
-		lblNewLabel_4.setForeground(new Color(255, 255, 255));
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_4.setIcon(new ImageIcon("C:\\images\\gestion\\Search-48.png"));
-		lblNewLabel_4.setBounds(10, 23, 236, 43);
-		panel_2.add(lblNewLabel_4);
+		JLabel lblSelectionClient = new JLabel("Selectionner un article");
+		lblSelectionClient.setOpaque(true);
+		lblSelectionClient.setBackground(new Color(255, 140, 0));
+		lblSelectionClient.setForeground(new Color(255, 255, 255));
+		lblSelectionClient.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblSelectionClient.setIcon(new ImageIcon("C:\\images\\gestion\\Search-48.png"));
+		lblSelectionClient.setBounds(10, 23, 236, 43);
+		panel_2.add(lblSelectionClient);
 		
 		JLabel lblCode = new JLabel("Code");
 		lblCode.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblCode.setBounds(275, 37, 31, 14);
 		panel_2.add(lblCode);
 		
-		textField_2 = new JTextField();
-		textField_2.setEnabled(false);
-		textField_2.setBounds(316, 35, 76, 20);
-		panel_2.add(textField_2);
-		textField_2.setColumns(10);
+		txtCode = new JTextField();
+		txtCode.setEnabled(false);
+		txtCode.setBounds(316, 35, 76, 20);
+		panel_2.add(txtCode);
+		txtCode.setColumns(10);
 		
-		JLabel lblCatgorie = new JLabel("Cat\u00E9gorie");
-		lblCatgorie.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblCatgorie.setBounds(402, 38, 59, 14);
-		panel_2.add(lblCatgorie);
+		JLabel lblCategorie = new JLabel("Cat\u00E9gorie");
+		lblCategorie.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblCategorie.setBounds(402, 38, 59, 14);
+		panel_2.add(lblCategorie);
 		
-		textField_3 = new JTextField();
-		textField_3.setEnabled(false);
-		textField_3.setBounds(471, 35, 101, 20);
-		panel_2.add(textField_3);
-		textField_3.setColumns(10);
+		txtCategorie = new JTextField();
+		txtCategorie.setEnabled(false);
+		txtCategorie.setBounds(471, 35, 101, 20);
+		panel_2.add(txtCategorie);
+		txtCategorie.setColumns(10);
 		
-		JLabel lblDsignation = new JLabel("D\u00E9signation");
-		lblDsignation.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblDsignation.setBounds(10, 77, 76, 14);
-		panel_2.add(lblDsignation);
+		JLabel lblDesignation = new JLabel("D\u00E9signation");
+		lblDesignation.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblDesignation.setBounds(10, 77, 76, 14);
+		panel_2.add(lblDesignation);
 		
-		textField_4 = new JTextField();
-		textField_4.setEnabled(false);
-		textField_4.setBounds(96, 75, 150, 20);
-		panel_2.add(textField_4);
-		textField_4.setColumns(10);
+		txtDesignation = new JTextField();
+		txtDesignation.setEnabled(false);
+		txtDesignation.setBounds(96, 75, 150, 20);
+		panel_2.add(txtDesignation);
+		txtDesignation.setColumns(10);
 		
 		JLabel lblMontant = new JLabel("Montant");
 		lblMontant.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblMontant.setBounds(256, 77, 59, 14);
 		panel_2.add(lblMontant);
 		
-		textField_5 = new JTextField();
-		textField_5.setEnabled(false);
-		textField_5.setBounds(316, 75, 76, 20);
-		panel_2.add(textField_5);
-		textField_5.setColumns(10);
+		txtMontant = new JTextField();
+		txtMontant.setEnabled(false);
+		txtMontant.setBounds(316, 75, 76, 20);
+		panel_2.add(txtMontant);
+		txtMontant.setColumns(10);
 		
-		JLabel lblNewLabel_5 = new JLabel("Quantit\u00E9");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_5.setBounds(402, 77, 59, 14);
-		panel_2.add(lblNewLabel_5);
+		JLabel lblQuantite = new JLabel("Quantit\u00E9");
+		lblQuantite.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblQuantite.setBounds(402, 77, 59, 14);
+		panel_2.add(lblQuantite);
 		
-		JSpinner spinner = new JSpinner();
-		spinner.setBounds(471, 75, 101, 20);
-		panel_2.add(spinner);
+		JSpinner spnQuantite = new JSpinner();
+		spnQuantite.setBounds(471, 75, 101, 20);
+		panel_2.add(spnQuantite);
 		
-		JLabel lblNewLabel_6 = new JLabel("Ajouter");
-		lblNewLabel_6.setIcon(new ImageIcon("C:\\images\\gestion\\Add-New-48.png"));
-		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_6.setBounds(96, 106, 115, 54);
-		panel_2.add(lblNewLabel_6);
+		JLabel lblAjouter = new JLabel("Ajouter");
+		lblAjouter.setIcon(new ImageIcon("C:\\images\\gestion\\Add-New-48.png"));
+		lblAjouter.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblAjouter.setBounds(96, 106, 115, 54);
+		panel_2.add(lblAjouter);
 		
 		JLabel lblModifier = new JLabel("Modifier");
 		lblModifier.setIcon(new ImageIcon("C:\\images\\gestion\\Data-Edit-48.png"));
@@ -268,43 +268,43 @@ public class Commande extends JFrame {
 		scrollPane.setBounds(10, 168, 562, 136);
 		panel_2.add(scrollPane);
 		
-		table = new JTable();
-		table.setEnabled(false);
-		table.setModel(new DefaultTableModel(
+		tblCommande = new JTable();
+		tblCommande.setEnabled(false);
+		tblCommande.setModel(new DefaultTableModel(
 			new Object[][] {
 			},
 			new String[] {
 				"Code", "Code Cat\u00E9gorie", "D\u00E9signation", "Quantit\u00E9", "Prix unitaire", "Total"
 			}
 		));
-		scrollPane.setViewportView(table);
+		scrollPane.setViewportView(tblCommande);
 		
-		JLabel lblModeDeRglement = new JLabel("Mode de r\u00E9glement");
-		lblModeDeRglement.setIcon(new ImageIcon("C:\\images\\gestion\\commande\\ATM-32.png"));
-		lblModeDeRglement.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblModeDeRglement.setBounds(365, 478, 166, 24);
-		contentPane.add(lblModeDeRglement);
+		JLabel lblModeDeReglement = new JLabel("Mode de r\u00E9glement");
+		lblModeDeReglement.setIcon(new ImageIcon("C:\\images\\gestion\\commande\\ATM-32.png"));
+		lblModeDeReglement.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblModeDeReglement.setBounds(365, 478, 166, 24);
+		contentPane.add(lblModeDeReglement);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setFont(new Font("Tahoma", Font.BOLD, 12));
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Carte Bleue", "Cheque", "Especes"}));
-		comboBox.setBounds(541, 481, 100, 20);
-		contentPane.add(comboBox);
+		JComboBox cbReglement = new JComboBox();
+		cbReglement.setFont(new Font("Tahoma", Font.BOLD, 12));
+		cbReglement.setModel(new DefaultComboBoxModel(new String[] {"Carte Bleue", "Cheque", "Especes"}));
+		cbReglement.setBounds(541, 481, 100, 20);
+		contentPane.add(cbReglement);
 		
-		JLabel lblNewLabel_7 = new JLabel("0.00 \u20AC");
-		lblNewLabel_7.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel lblTotal = new JLabel("0.00 \u20AC");
+		lblTotal.setHorizontalAlignment(SwingConstants.CENTER);
 		Border border = BorderFactory.createLineBorder(Color.ORANGE, 2);
-		lblNewLabel_7.setBorder(border);
-		lblNewLabel_7.setBackground(new Color(255, 140, 0));
-		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_7.setBounds(747, 473, 75, 35);
-		lblNewLabel_7.setOpaque(true);
-		contentPane.add(lblNewLabel_7);
+		lblTotal.setBorder(border);
+		lblTotal.setBackground(new Color(255, 140, 0));
+		lblTotal.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblTotal.setBounds(747, 473, 75, 35);
+		lblTotal.setOpaque(true);
+		contentPane.add(lblTotal);
 		
-		JLabel lblValiderLaCommande_1 = new JLabel("Valider la commande");
-		lblValiderLaCommande_1.setIcon(new ImageIcon("C:\\images\\gestion\\commande\\Shopping-Cart-05-48.png"));
-		lblValiderLaCommande_1.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblValiderLaCommande_1.setBounds(637, 536, 185, 35);
-		contentPane.add(lblValiderLaCommande_1);
+		JLabel lblValide = new JLabel("Valider la commande");
+		lblValide.setIcon(new ImageIcon("C:\\images\\gestion\\commande\\Shopping-Cart-05-48.png"));
+		lblValide.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblValide.setBounds(637, 536, 185, 35);
+		contentPane.add(lblValide);
 	}
 }
