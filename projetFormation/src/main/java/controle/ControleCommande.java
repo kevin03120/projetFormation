5a173242002786cd9f20c7e7df497161372d038e
Kevin <kevin.malleret03@gmail.com>
2017-07-27 11:37:19 +0200

    Debut controle commande

--- projetFormation/src/main/java/controle/ControleCommande.java
+++ projetFormation/src/main/java/controle/ControleCommande.java
@@ -3,11 +3,34 @@ package src.main.java.controle;
 import java.util.List;
 
 import src.main.java.controle.connexion.GlobalConnection;
+import src.main.java.controle.modele.ModeleDynamiqueClient;
 import src.main.java.controle.modele.ModeleDynamiqueCommande;
 import src.main.java.entite.Client;
-import src.main.java.entite.dao.ClientDaoMysql;
+import src.main.java.entite.Commande;
 import src.main.java.entite.dao.CommandeDaoMysql;
 
 public class ControleCommande {
 	
+	private CommandeDaoMysql commandeDao;
+	private ModeleDynamiqueCommande modeleCommande;
+	private List<Commande> mesCommandes;
+
+	public ControleCommande() {
+		commandeDao = new CommandeDaoMysql(GlobalConnection.getInstance());
+		mesCommandes = commandeDao.getAllCommandes();
+		modeleCommande = new ModeleDynamiqueCommande(mesCommandes);
+	}
+	
+
+	public ModeleDynamiqueCommande getModeleCommande() {
+		return modeleCommande;
+	}
+
+	public List<Commande> getMesCommandes() {
+		return mesCommandes;
+	}
+	
+	public double getTotalPrixTTC(){
+		return commandeDao.getTotalPrixCommandes();
+	}
 }
